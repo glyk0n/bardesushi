8e2738ded2f9e41e5e81b326447022b0bb50fecd
madhy2016 <heravada@gmail.com>
2019-05-19 18:33:09 +0200

    primer commit

---
+++ src/AppBundle/Controller/HomeController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use AppBundle\Entity\Producto;
+
+class HomeController extends Controller
+{
+	/**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        return $this->render('@App/home/index.html.twig', array(
+            // ...
+        ));
+    }
+
+    /**
+     * @Route("/menu", name="menupage")
+     */
+    public function menuAction()
+    {
+    	//Captura del repo contra la BD. $em x $repository
+    	$em = $this->getDoctrine()->getRepository(Producto::class);
+
+    	//findAll productos
+    	$productos = $em->findAll();
+
+        return $this->render('@App/home/menu.html.twig', array(
+        	'producto' => $productos
+        ));
+    }
+}
